b2928bc0a77194f27c45d7db783c6305fa7bae83
AJstylishhh <61421802+AJstylishhh@users.noreply.github.com>
2026-08-29 19:33:33 +0500

    Add ZeroTier socket bridge interface for LDN

---
+++ scripts/zt_bridge.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <stdint.h>
+#include <stddef.h>
+
+namespace ztbridge {
+
+struct PollFd {
+    int fd;
+    short events;
+    short revents;
+};
+
+enum {
+    ZT_POLLIN = 0x001,
+    ZT_POLLPRI = 0x040,
+    ZT_POLLOUT = 0x004,
+    ZT_POLLERR = 0x008,
+    ZT_POLLNVAL = 0x020,
+    ZT_POLLHUP = 0x010
+};
+
+int init();
+int socket(int family, int type, int protocol);
+int bind(int fd, const sockaddr_in *addr);
+int listen(int fd, int backlog);
+int accept(int fd);
+int connect(int fd, const sockaddr_in *addr);
+int close(int fd);
+ssize_t send(int fd, const void *buf, size_t len);
+ssize_t sendto(int fd, const void *buf, size_t len, const sockaddr_in *addr);
+ssize_t recv(int fd, void *buf, size_t len);
+int poll(PollFd *fds, size_t nfds, int timeout_ms);
+int local_ip_host_order(uint32_t *out);
+uint32_t peer_ip_host_order();
+
+}
